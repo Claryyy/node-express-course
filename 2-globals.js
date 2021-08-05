8fb345a702e859d2c6fc453280dab009d6bd3b89
Clarisse <78910159+Claryyy@users.noreply.github.com>
2021-08-05 16:31:14 +0100

    First commit

---
+++ 2-globals.js
@@ -0,0 +1,7 @@
+//GLOBALS - NO WINDOW !!!
+
+// __dirname - path to current directory
+// __filename - filename
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+// process - infor about where the program is being executed
---
+++ 3-modules.js
@@ -0,0 +1,5 @@
+// Modules - Encapsulated code (only share minimum)
+// CommonJS - every file is module (by default)
+
+sayHi("susan");
+sayHi(john);
