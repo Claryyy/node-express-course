5ed6ed29e7d858ba7dab6789af14d2d29ab25499
Clarisse <78910159+Claryyy@users.noreply.github.com>
2021-08-05 17:22:28 +0100

    Modules (os, path, fs)

---
+++ 11-fs-async.js
@@ -0,0 +1,34 @@
+const { readFile, writeFile } = require("fs");
+
+//async so need callback
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+  // if we don't provide the utf coding you get a buffer
+
+  const first = result;
+  readFile("./content/second.txt", "utf8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(result);
+    // if we don't provide the utf coding you get a buffer
+
+    const second = result;
+    writeFile(
+      "./content/result-sync.txt",
+      `Here is the result: ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result);
+      }
+    );
+  });
+});
