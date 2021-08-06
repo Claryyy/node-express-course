59eef6abd6797dfb82f33dcdb803de756d212c32
Clarisse <78910159+Claryyy@users.noreply.github.com>
2021-08-06 10:28:40 +0100

    Http basics

---
+++ 2-express-tutorial/app.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.method);
+  console.log(req.url);
+
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write("<h1>home page</h1>");
+  res.end();
+});
+
+server.listen(5000);
