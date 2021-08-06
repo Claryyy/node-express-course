5f547d72ec6ccc52a1a4b4b88db36587eb1d45af
Clarisse <78910159+Claryyy@users.noreply.github.com>
2021-08-06 17:30:42 +0100

    Express router

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,25 +1,80 @@
 const express = require("express");
 const app = express();
+let { people } = require("./data");
 
-// req => middleware => res
+// static assets
+app.use(express.static("./methods-public"));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
 
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear;
-  console.log(method, url, time);
-  //always always either you terminate by sending back your own response or you pass it on to the next middleware
-  next();
-};
-
-app.get("/", logger, (req, res) => {
-  res.send("Home");
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
-app.get("/about", logger, (req, res) => {
-  res.send("About");
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
+  }
+  res.status(201).json({ success: true, person: name });
 });
 
-app.listen(5000, (req, res) => {
-  console.log("Server is listening on port 5000...");
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
+  }
+  res.status(201).json({ success: true, data: [...people, name] });
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+
+  res.status(401).send("Please Provide Credentials");
+});
+
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+});
+
+app.delete("/api/people/:id", (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` });
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id)
+  );
+  return res.status(200).json({ success: true, data: newPeople });
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000....");
 });
