250f75d29b9a4c52e9892ee1cc6937e451c90b17
Clarisse <78910159+Claryyy@users.noreply.github.com>
2021-08-06 11:31:47 +0100

    Express api calls and date retrieving

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,12 +1,32 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const { products } = require("./data");
 
-const server = http.createServer((req, res) => {
-  console.log(req.method);
-  console.log(req.url);
-
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>home page</h1>");
-  res.end();
+app.get("/", (req, res) => {
+  // res.json(products);
+  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
 });
 
-server.listen(5000);
+api.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+api.get("/api/products/1", (req, res) => {
+  const singleProduct = products.find((product) => product.id === 1);
+  res.json(singleProduct);
+});
+
+api.get("/api/products/:productID", (req, res) => {
+  console.log(req);
+  console.log(req.params);
+  const singleProduct = products.find((product) => product.id === 1);
+  res.json(singleProduct);
+});
+
+app.listen(5000, (req, res) => {
+  console.log("Server is listening on port 5000...");
+});
